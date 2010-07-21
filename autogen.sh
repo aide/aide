d003ab0f51cd5949882c0eb61ef280394f2c380a
Hannes von Haugwitz <hannes@vonhaugwitz.com>
2010-07-21 08:57:23 +0200

    autogen.sh: adjusted prompt for ./configure & make
    Prompt for ./configure and make only if autoreconf was successful

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-autoreconf -fv --install
-echo "You can now run \"./configure\" and then \"make\"."
+autoreconf -fv --install && echo "You can now run \"./configure\" and then \"make\"."

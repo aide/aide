ed84193fd347dfc1f23a78b1a139cd9f068dd7c2
Hannes von Haugwitz <hannes@vonhaugwitz.com>
2022-05-26 14:07:09 +0200

    Add --always option to git describe in version.sh

--- version.sh
+++ version.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if GIT_VERSION=`git describe`; then
+if GIT_VERSION=$(git describe --always); then
     echo "m4_define([AIDE_VERSION], [${GIT_VERSION#v}])" > version.m4.$$
     mv version.m4.$$ version.m4
     rm -f version.m4.$$
 else
-    echo "Error: git describe failed"
+    echo "Error: 'git describe --always' failed"
     exit 1
 fi

574ae12bdddd40d76432177d6de3296c7afc2c13
Hannes von Haugwitz <hannes@vonhaugwitz.com>
2023-02-06 21:59:53 +0100

    Update copyright notice

--- src/conf_eval.c
+++ src/conf_eval.c
@@ -1,7 +1,7 @@
 /*
  * AIDE (Advanced Intrusion Detection Environment)
  *
- * Copyright (C) 2019-2022 Hannes von Haugwitz
+ * Copyright (C) 2019-2023 Hannes von Haugwitz
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as

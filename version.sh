f445264ff5cd61a932185182d035588c89af5f51
Marc 'Zugschlus' Haber <mh+github@zugschlus.de>
2024-07-08 14:17:31 +0200

    Allow version.sh to accept GIT_VERSION from environment
    This makes it easier to package development snapshots with a defined version number

--- version.sh
+++ version.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-if GIT_VERSION=$(git describe --always); then
-    echo "m4_define([AIDE_VERSION], [${GIT_VERSION#v}])" > version.m4.$$
-    mv version.m4.$$ version.m4
-    rm -f version.m4.$$
-else
-    echo "Error: 'git describe --always' failed"
-    exit 1
+if [ -z "$GIT_VERSION" ]; then
+    if ! GIT_VERSION=$(git describe --always); then
+        echo "Error: 'git describe --always' failed"
+        exit 1
+    fi
 fi
+
+echo "m4_define([AIDE_VERSION], [${GIT_VERSION#v}])" > version.m4.$$
+mv version.m4.$$ version.m4
+rm -f version.m4.$$
